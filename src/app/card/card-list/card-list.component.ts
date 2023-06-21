5af2b6c01b624a3c91284037d93543fbf4cae968
GrazielyTukozaki <grazielybasilio@hotmail.com>
2023-06-20 23:17:20 -0300

    add service to fecht data and fetch data to components

--- src/app/card/card-list/card-list.component.ts
+++ src/app/card/card-list/card-list.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Character } from 'src/app/models/character.model';
+import { Component, HostListener, Inject } from '@angular/core';
 import { CardItemComponent } from './card-item/card-item.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { CharactersService } from 'src/app/services/characters.service';
+import { SearchBarComponent } from '../../shared/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.scss'],
   standalone: true,
-  imports: [CommonModule, CardItemComponent],
+  imports: [CommonModule, CardItemComponent, SearchBarComponent],
 })
 export class CardListComponent {
-  public readonly character$!: Observable<Character>;
+  public characters$ = this.charactersService.characters$;
+
+  constructor(private charactersService: CharactersService) {}
 }
